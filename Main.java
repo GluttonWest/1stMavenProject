a34b72c853a7e41eee1ffd17c37f5a28d00b8d91
Adhirath <adhirathkatyarmal@gmail.com>
2025-11-25 15:29:50 +0530

    2nd

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! im in danger!!");
+        System.out.println("Hello, World! im adhirath!!");
     }
 }
